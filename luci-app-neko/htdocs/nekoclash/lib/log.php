3bcff52315818ed88f6a04baa13c93e0f70a4123
nosignals <dimasvito100@gmail.com>
2024-01-28 00:54:01 +0700

    readable binary log

--- luci-app-neko/htdocs/nekoclash/lib/log.php
+++ luci-app-neko/htdocs/nekoclash/lib/log.php
@@ -6,6 +6,6 @@ $neko_bin_log_path="$neko_dir/tmp/neko_log.txt";
 if(isset($_GET['data'])){
     $dt = $_GET['data'];
     if ($dt == 'neko') echo shell_exec("cat $neko_log_path");
-    else if($dt == 'bin') echo shell_exec("cat $neko_bin_log_path");
+    else if($dt == 'bin') echo shell_exec("cat $neko_bin_log_path | awk -F'[\"T.= ]' '{print \"[ \" $4 \" ] \" $8 \" :\", substr($0,index($0,$11))}' | sed 's/.$//'");
 }
 ?>
